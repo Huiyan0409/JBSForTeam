08fa1fc49b55a7804785aebe231b72842b52262c
Huiyan Zhang <nicolezhang@brandeis.edu>
2019-06-27 13:33:02 -0400

    Answer Question Part(uncomplete)

---
+++ models/Answer.js
@@ -0,0 +1,14 @@
+'use strict';
+const mongoose = require( 'mongoose' );
+const Schema = mongoose.Schema;
+const ObjectId = mongoose.Schema.Types.ObjectId;
+
+var answerSchema = Schema( {
+  userId: ObjectId,
+  questionId: ObjectId,
+  userName: String,
+  answer: String,
+  createdAt: Date
+} );
+
+module.exports = mongoose.model( 'AnswerPost', answerSchema );
---
+++ models/Question.js
@@ -0,0 +1,13 @@
+'use strict';
+const mongoose = require( 'mongoose' );
+const Schema = mongoose.Schema;
+const ObjectId = mongoose.Schema.Types.ObjectId;
+
+var questionSchema = Schema( {
+  userId: ObjectId,
+  userName: String,
+  question: String,
+  createdAt: Date
+} );
+
+module.exports = mongoose.model( 'QuestionPost', questionSchema );
