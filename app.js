1e66eb3785b7197f64c22485f096a5b00de36774
Hangyu <supremeethan@brandeis.edu>
2019-06-27 10:21:15 -0400

    confliction test

--- app.js
+++ app.js
@@ -45,7 +45,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-1231231
 
 /*************************************************************************
      HERE ARE THE AUTHENTICATION ROUTES
