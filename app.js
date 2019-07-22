a9a86c7c5272adcd907bcbdb66d315c30d661a0a
Richard Li <richardli@brandeis.edu>
2019-07-21 21:44:44 -0400

    Tutor Rating Profile Update

--- app.js
+++ app.js
@@ -348,7 +348,10 @@ app.get('/editMyTutorProfile/:id', isLoggedIn, tutorController.showOldTutorProfi
 app.post('/updateTutorProfile/:id', isLoggedIn, tutorController.updateTutorProfile)
 
 //tutor rating
-app.post('/tutorRatings/:id', isLoggedIn, tutorController.updateTutorRatingProfile)
+app.post('/tutorRatings/:id', isLoggedIn, tutorController.showTutorRatingProfile)
+
+//tutor rating
+app.post('/updateTutorRatings/:id', isLoggedIn, tutorController.updateTutorRatingProfile)
 
 //tutor tutorRegister
 app.get('/tutorRegister', function(req, res, next){
