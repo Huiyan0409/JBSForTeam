86716f34e3ef733b57a2fa246be3794da66249dc
Hangyu <supremeethan@brandeis.edu>
2019-07-29 14:24:19 -0400

    change iclaster to claster

--- app.js
+++ app.js
@@ -100,7 +100,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 const approvedLogins = ["supremeethan@brandeis.edu", "kaimingwang@brandeis.edu", "yhao@brandeis.edu", "richardli@brandeis.edu", "nicolezhang@brandeis.edu"];
 // here is where we check on their logged in status
 app.use((req,res,next) => {
-  res.locals.title="iClaster"
+  res.locals.title="Claster"
   res.locals.loggedIn = false
   if (req.isAuthenticated()){
     if (req.user.googleemail.endsWith("edu") ||
