4001151add072f9288188b1ebf5ad5294667dd5b
Hangyu <supremeethan@brandeis.edu>
2019-10-31 22:58:59 -0400

    1

--- controllers/tutorController.js
+++ controllers/tutorController.js
@@ -234,7 +234,6 @@ exports.updateAppointment = (req, res, next) => {
             status: "incomplete"
         }
     );
-
     newAppointment.save()
         .then(() => {
             send_email(req, res);
@@ -250,16 +249,12 @@ function send_email(req, res) {
     const sgMail = require('@sendgrid/mail');
     sgMail.setApiKey(process.env.SENDGRID_API_KEY);
     const msg = {
-        to: '' + res.locals.user.googleemail,
-        from: 'iclaster support team',
+        to: res.locals.user.googleemail,
+        from: 'bbdhy96@gmail.com',
         subject: 'Your iClaster tutor appointment is set',
-        text: 'Hi, your appointment with ' + req.body.tuteeName + ' is set on '
-        + req.body.startAt,
+        text: 'Hi, your appointment with ' + req.body.tuteeName + ' is set',
         html: '<strong>and easy to do anywhere, even with Node.js</strong>',
     };
-    console.log(res.locals.user.googleemail);
-    console.log(req.body.tuteeName);
-    console.log(req.body.startAt);
     sgMail.send(msg);
 }
 
